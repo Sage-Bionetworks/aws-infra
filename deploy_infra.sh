6fd7d3e3d597b9ddf4dc2c7953061c0f53245361
Khai Do <zaro0508@gmail.com>
2018-02-10 11:12:39 -0800

    Prepare for CI
    * setup cloudformation bucket in bootstrap template
    * add comman templates for setting up essential resources
    * setup for travis to deploy templates to an S3 bucket for other
      projects to reference.

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
+FILES=cf_templates/*
+for f in $FILES
+do
+  echo -e "\nValidating CF template $f"
+  aws cloudformation validate-template --template-body file://$f
+done
 
-S3_TARGET_DIR="s3://bridge-cloudformation-artifacts-$AWS_ACCOUNT_ID/$STACK_NAME/"
+S3_TARGET_DIR="s3://bootstrap-awss3cloudformationbucket-19qromfd235z9/$TRAVIS_BRANCH/"
 echo -e "\nUploading cf_templates to $S3_TARGET_DIR"
 aws s3 cp --recursive cf_templates $S3_TARGET_DIR
-
-TEMPLATE_URL="https://s3.amazonaws.com/bridge-cloudformation-artifacts-$AWS_ACCOUNT_ID/$STACK_NAME/bridge.yml"
-echo -e "\nValidating CF template $TEMPLATE_URL"
-aws cloudformation validate-template --template-url $TEMPLATE_URL
-
-echo -e "\nDeploying CF template $TEMPLATE_URL"
-# Handle message that shouldn't be an error, https://github.com/hashicorp/terraform/issues/5653
-UPDATE_CMD="./update_cf_stack.sh $TEMPLATE_URL"
-message=$($UPDATE_CMD 2>&1 1>/dev/null)
-error_code=$(echo $?)
-if [[ $error_code -ne 0 && $message =~ .*"No updates are to be performed".* ]]; then
-  echo -e "\nNo stack changes detected. An update is not required."
-  error_code=0
-else
-  echo $message
-fi
-exit $error_code
