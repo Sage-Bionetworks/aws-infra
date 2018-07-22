5a2cdd2a7b01dbc1051dcf67bee9eda490d852f9
Khai Do <zaro0508@gmail.com>
2018-07-21 21:29:55 -0700

    update scripts to deploy templates and lambdas

--- deploy-templates.sh
+++ deploy-templates.sh
@@ -3,7 +3,7 @@ set -e
 
 # Deploy templates to AWS Admincentral account to share with other projects.
 REPO_NAME="${PWD##*/}"
-S3_BUCKET="bootstrap-awss3cloudformationbucket-19qromfd235z9"
+S3_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='us-east-1-bootstrap-CloudformationBucket'].Value" --output text)
 S3_BUCKET_PATH="$REPO_NAME/$TRAVIS_BRANCH"
 S3_BUCKET_URL="s3://$S3_BUCKET/$S3_BUCKET_PATH"
 
