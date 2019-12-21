7156be864cab9d722e9698e926d517b2c0d54ce2
Khai Do <zaro0508@gmail.com>
2019-12-21 15:01:19 -0800

    Add Parameter handling
    The current implementation of the Explode macro only handeld creating
    multiple resources from mapping values.  This is an addition to handle
    generating multiple resources for parameter lists.
    
    * add new explode parameter feature
    * add documentation
    * add unit tests

---
+++ lambdas/cfn-explode-macro/tests/unit/test_app.py
@@ -0,0 +1,121 @@
+import json
+import jsondiff
+import unittest
+from explode.app import handler
+
+
+class test_app(unittest.TestCase):
+
+    def test_no_explode(self):
+        event = \
+            {
+                "requestId": "testRequest",
+                "templateParameterValues": {},
+                "region": "us-east-1"
+            }
+
+        self.event = event
+
+        with open(r'test_no_explode.json') as file:
+            test_template = json.load(file)
+        self.event["fragment"] = test_template
+        result = handler(self.event, None)
+        fragment = result["fragment"]
+
+        with open(r'expected_no_explode.json') as file:
+            expected_template = json.load(file)
+
+        res = jsondiff.diff(fragment, expected_template)
+        self.assertEqual(res, {})
+
+    def test_explode_param(self):
+        event = \
+            {
+                "requestId": "testRequest",
+                "templateParameterValues": {
+                    "Ports": [22, 80]
+                },
+                "region": "us-east-1"
+            }
+
+        self.event = event
+
+        with open(r'test_explode_param.json') as file:
+            test_template = json.load(file)
+        self.event["fragment"] = test_template
+        result = handler(self.event, None)
+        fragment = result["fragment"]
+
+        with open(r'expected_explode_param.json') as file:
+            expected_template = json.load(file)
+
+        res = jsondiff.diff(fragment, expected_template)
+        self.assertEqual(res, {})
+
+    def test_explode_map(self):
+        event = \
+            {
+                "requestId": "testRequest",
+                "templateParameterValues": {},
+                "region": "us-east-1"
+            }
+
+        self.event = event
+
+        with open(r'test_explode_map.json') as file:
+            test_template = json.load(file)
+        self.event["fragment"] = test_template
+        result = handler(self.event, None)
+        fragment = result["fragment"]
+
+        with open(r'expected_explode_map.json') as file:
+            expected_template = json.load(file)
+
+        res = jsondiff.diff(fragment, expected_template)
+        self.assertEqual(res, {})
+
+    def test_explode_no_match(self):
+        event = \
+            {
+                "requestId": "testRequest",
+                "templateParameterValues": {},
+                "region": "us-east-1"
+            }
+
+        self.event = event
+
+        with open(r'test_explode_no_match.json') as file:
+            test_template = json.load(file)
+        self.event["fragment"] = test_template
+        result = handler(self.event, None)
+        fragment = result["fragment"]
+
+        with open(r'expected_explode_no_match.json') as file:
+            expected_template = json.load(file)
+
+        res = jsondiff.diff(fragment, expected_template)
+        self.assertEqual(res, {})
+
+    def test_explode_map_and_param(self):
+        event = \
+            {
+                "requestId": "testRequest",
+                "templateParameterValues": {
+                    "Ports": [22, 80]
+                },
+                "region": "us-east-1"
+            }
+
+        self.event = event
+
+        with open(r'test_explode_map_and_param.json') as file:
+            test_template = json.load(file)
+        self.event["fragment"] = test_template
+        result = handler(self.event, None)
+        fragment = result["fragment"]
+
+        with open(r'expected_explode_map_and_param.json') as file:
+            expected_template = json.load(file)
+
+        res = jsondiff.diff(fragment, expected_template)
+        self.assertEqual(res, {})
