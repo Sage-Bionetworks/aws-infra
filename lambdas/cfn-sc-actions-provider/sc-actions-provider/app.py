3db0fe0fd8426f22d396e04a63d7276a64b62660
Khai Do <zaro0508@gmail.com>
2020-04-04 13:15:24 -0700

    add method to update SC action

--- lambdas/cfn-sc-actions-provider/sc-actions-provider/app.py
+++ lambdas/cfn-sc-actions-provider/sc-actions-provider/app.py
@@ -7,7 +7,7 @@ from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
 helper = CfnResource(
-    json_logging=False, log_level='DEBUG', boto_level='CRITICAL')
+    json_logging=False, log_level='INFO', boto_level='CRITICAL')
 
 try:
     sc = boto3.client("servicecatalog")
@@ -35,7 +35,7 @@ def create_provider(aws_account_id, name, ssm_doc_name, ssm_doc_version, assume_
                   }
         )
         resource_id = response['ServiceActionDetail']['ServiceActionSummary']['Id']
-        logger.info("resource_id = " + resource_id)
+        logger.info("created sc action " + id)
         return resource_id
     except ClientError as e:
         raise e
@@ -50,10 +50,37 @@ def delete(event, context):
     logger.debug("Received event: " + json.dumps(event, sort_keys=False))
     try:
         id = event['PhysicalResourceId']
-        logger.info("resource id = " + id)
+        logger.info("deleting sc action " + id)
         sc.delete_service_action(
             Id=id
         )
+    except ClientError as e:
+        raise e
+
+@helper.update
+def update(event, context):
+    logger.debug("Received event: " + json.dumps(event, sort_keys=False))
+
+    try:
+        new_properties = event['ResourceProperties']
+        old_properties = event['OldResourceProperties']
+        id = event['PhysicalResourceId']
+        if new_properties != old_properties:
+            logger.info("updating sc action " + id)
+            response = sc.update_service_action(
+                Id=id,
+                Name=new_properties['Name'],
+                Definition= {
+                        "Name": new_properties['SsmDocName'],
+                        "Version": new_properties['SsmDocVersion'],
+                        "AssumeRole": new_properties['AssumeRole'],
+                        "Parameters": "[{\"Name\":\"AutomationAssumeRole\",\"Type\":\"TARGET\"}]"
+                      }
+            )
+            resource_id = response['ServiceActionDetail']['ServiceActionSummary']['Id']
+            logger.info("resource_id = " + resource_id)
+            id=resource_id
+        return id
     except ClientError as e:
         raise e
 
