064e04d59a8c7cfdfb3e564ca3d439d537beaa4a
Khai Do <zaro0508@gmail.com>
2020-04-07 20:42:01 -0700

    add error handling

--- lambdas/cfn-sc-actions-provider/sc-actions-provider/app.py
+++ lambdas/cfn-sc-actions-provider/sc-actions-provider/app.py
@@ -3,11 +3,10 @@ import json
 import logging
 
 from crhelper import CfnResource
-from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
 helper = CfnResource(
-    json_logging=False, log_level='INFO', boto_level='CRITICAL')
+    json_logging=False, log_level='DEBUG', boto_level='CRITICAL')
 
 try:
     sc = boto3.client("servicecatalog")
@@ -21,6 +20,21 @@ def get_parameters(event):
     ssm_doc_version = event['ResourceProperties']['SsmDocVersion']
     assume_role = event['ResourceProperties']['AssumeRole']
     return aws_account_id, name, ssm_doc_name, ssm_doc_version, assume_role
+
+def validate_parameters(aws_account_id, name, ssm_doc_name, ssm_doc_version, assume_role):
+    logger.debug("Validate SSM doc " + ssm_doc_name + ", " + ssm_doc_version)
+    ssm = boto3.client("ssm")
+    ssm.get_document(
+        Name=ssm_doc_name,
+        DocumentVersion=ssm_doc_version,
+    )
+
+    assume_role_name = assume_role.split(':')[5].split('/')[1]
+    logger.debug("Validate role: " + assume_role_name)
+    iam = boto3.client("iam")
+    iam.get_role(
+        RoleName=assume_role_name
+    )
 
 def create_provider(aws_account_id, name, ssm_doc_name, ssm_doc_version, assume_role):
     response = sc.create_service_action(
@@ -40,6 +54,7 @@ def create_provider(aws_account_id, name, ssm_doc_name, ssm_doc_version, assume_
 @helper.create
 def create(event, context):
     logger.debug("Received event: " + json.dumps(event, sort_keys=False))
+    validate_parameters(*get_parameters(event))
     return create_provider(*get_parameters(event))
 
 @helper.delete
--- lambdas/cfn-sc-actions-provider/sc-actions-provider/associate.py
+++ lambdas/cfn-sc-actions-provider/sc-actions-provider/associate.py
@@ -1,12 +1,13 @@
 import boto3
 import json
 import logging
+import requests
 
 from crhelper import CfnResource
 
 logger = logging.getLogger(__name__)
 helper = CfnResource(
-    json_logging=False, log_level='INFO', boto_level='CRITICAL')
+    json_logging=False, log_level='DEBUG', boto_level='CRITICAL')
 
 try:
     sc = boto3.client("servicecatalog")
@@ -20,21 +21,35 @@ def get_parameters(event):
     provisioning_artifact_id = event['ResourceProperties']['ProvisioningArtifactId']
     return aws_account_id, service_action_id, product_id, provisioning_artifact_id
 
+def validate_parameters(aws_account_id, service_action_id, product_id, provisioning_artifact_id):
+    sc.describe_provisioning_artifact(
+        ProductId=product_id,
+        ProvisioningArtifactId=provisioning_artifact_id
+    )
+
 def create_provider(aws_account_id, service_action_id, product_id, provisioning_artifact_id):
-    response = sc.batch_associate_service_action_with_provisioning_artifact(
-        ServiceActionAssociations=[
-            {
-                'ServiceActionId': service_action_id,
-                'ProductId': product_id,
-                'ProvisioningArtifactId': provisioning_artifact_id
-            },
-        ],
+
+    logger.debug(
+        "Associate action " + service_action_id + " with product " + product_id + ", " + provisioning_artifact_id)
+    response = sc.associate_service_action_with_provisioning_artifact(
+        ProductId=product_id,
+        ServiceActionId=service_action_id,
+        ProvisioningArtifactId=provisioning_artifact_id
     )
 
 @helper.create
 def create(event, context):
     logger.debug("Received event: " + json.dumps(event, sort_keys=False))
-    return create_provider(*get_parameters(event))
+    validate_parameters(*get_parameters(event))
+    if create_provider(*get_parameters(event)):
+        return True
+    else:
+        logger.debug("Send cloudformation a FAILED response")
+        status = 'FAILED'
+        reason = "Failed to create a sevice catalog association"
+        data = {}
+        send_response(event, context, status, reason, data)
+        return False
 
 @helper.delete
 def delete(event, context):
@@ -42,7 +57,7 @@ def delete(event, context):
     disassociate_action(*get_parameters(event))
 
 def disassociate_action(aws_account_id, service_action_id, product_id, provisioning_artifact_id):
-    response = sc.batch_disassociate_service_action_from_provisioning_artifact(
+    response = sc.disassociate_service_action_from_provisioning_artifact(
         ServiceActionAssociations=[
             {
                 'ServiceActionId': service_action_id,
@@ -63,7 +78,7 @@ def reassociate_action(event):
     old_properties = event['OldResourceProperties']
     if new_properties != old_properties:
         logger.info("removing association " + old_properties['ServiceActionId'])
-        remove_response = sc.batch_disassociate_service_action_from_provisioning_artifact(
+        remove_response = sc.disassociate_service_action_from_provisioning_artifact(
             ServiceActionAssociations=[
                 {
                     'ServiceActionId': old_properties['ServiceActionId'],
@@ -74,7 +89,7 @@ def reassociate_action(event):
         )
 
         logger.info("adding association " + new_properties['ServiceActionId'])
-        add_response = sc.batch_associate_service_action_with_provisioning_artifact(
+        add_response = sc.associate_service_action_with_provisioning_artifact(
             ServiceActionAssociations=[
                 {
                     'ServiceActionId': new_properties['ServiceActionId'],
@@ -83,6 +98,23 @@ def reassociate_action(event):
                 },
             ],
         )
+
+def send_response(event, context, status, reason, data):
+    responseBody = {'Status': status,
+                    'Reason': reason,
+                    'StackId': event['StackId'],
+                    'RequestId': event['RequestId'],
+                    'LogicalResourceId': event['LogicalResourceId'],
+                    'Data': data}
+    logger.debug('RESPONSE BODY:n' + json.dumps(responseBody))
+    try:
+        req = requests.put(event['ResponseURL'], data=json.dumps(responseBody))
+        if req.status_code != 200:
+            logger.debug(req.text)
+            raise Exception('Recieved non 200 response while sending response to CFN.')
+        return
+    except requests.exceptions.RequestException as e:
+        raise
 
 def lambda_handler(event, context):
     helper(event, context)
